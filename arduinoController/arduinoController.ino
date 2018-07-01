766719e185e648c01cbf3af1c2e84503fa8a0fd1
Tiago Martins <tiaguinhomartins_@hotmail.com>
2018-07-01 23:38:21 +0100

    Current ECDH exchange code state.

---
+++ arduinoController/arduinoController.ino
@@ -0,0 +1,258 @@
+#define MAX_MSG_LEN 100
+
+#include <AltSoftSerial.h>
+#include <Base64.h>
+#include <Arduino.h>
+#include <RNG.h>
+#include <string.h>
+#include <Crypto.h>
+#include <Curve25519.h>
+#include <AES.h>
+#include <GCM.h>
+
+// no message sent to the arduino can ever have more than this many bytes
+// used to assume size of internal buffers
+AltSoftSerial altSerial;
+const int pin_accept = 13;
+const int pin_decline = 8;
+
+String smartphonesArray[2] = {"uXUi27eQpTCOaB8DfHgD", "randomkeyvalue"};
+String string_received = "";
+String string_ok = "ok";
+bool can_read = true;
+bool received_smartphone_pub_key = false;
+bool received_encrypted_msg = false;
+String full_encoded_msg = "";
+uint8_t two_halfs = 0;
+
+// ECDH arduino public and private keys, smartphone public key.
+// The smartphone public key array is later used to store the shared key
+uint8_t arduino_secret_key[32];
+uint8_t arduino_public_key[32];
+uint8_t smartphone_public_key[32];
+
+void blink(int pin_out) {
+	digitalWrite(pin_out, HIGH);
+	delay(250);
+	digitalWrite(pin_out, LOW);
+	delay(250);
+	digitalWrite(pin_out, HIGH);
+	delay(250);
+	digitalWrite(pin_out, LOW);
+}
+
+void dealWithDisconnection(){
+	received_smartphone_pub_key = false;
+	received_encrypted_msg = false;
+	memset(arduino_secret_key, 0, sizeof(arduino_secret_key));
+	memset(arduino_public_key, 0, sizeof(arduino_public_key));
+	memset(smartphone_public_key, 0, sizeof(smartphone_public_key));
+}
+
+void generateSharedSecret(){
+	//Serial.print(F("Generating shared secret: "));
+	Curve25519::dh2(smartphone_public_key, arduino_secret_key);
+	/*for(uint8_t i = 0; i< 32; i++){
+		Serial.print(smartphone_public_key[i]);
+		Serial.print(F(", "));
+	}
+	Serial.println();*/
+}
+
+void generateECDHkeyValues(){
+	//Serial.print(F("ECDH: "));
+	Curve25519::dh1(arduino_public_key, arduino_secret_key);
+	/*
+	for(int j = 0; j<32; j++){
+		Serial.print(arduino_secret_key[j]);
+		Serial.print(F(", "));
+	}
+	Serial.println();
+	
+	for(int j = 0; j<32; j++){
+		Serial.print(arduino_public_key[j]);
+		Serial.print(F(", "));
+	}
+	Serial.println();
+	*/
+}
+
+void sendArduinoPubKey(){
+	//Serial.println(F("Sending pub key.."));
+	char to_base_64[32];
+	for(uint8_t i = 0; i<32; i++){
+		to_base_64[i] = char(arduino_public_key[i]);
+	}
+	int encodedLen = base64_enc_len(32);
+	char encoded[encodedLen];
+	base64_encode(encoded, to_base_64, 32);
+	//Serial.println(encoded);	
+	altSerial.println(encoded);
+}
+
+void dealWithSmartphoneKey(char* sp_pub_key){
+	//base64_decode((char*)smartphone_public_key, string_array, sp_pub_key_length);
+	//Serial.println(F("Dealing with sp pub key"));
+	//int sp_pub_key_length = sp_pub_key.length();
+	int sp_pub_key_length = strlen(sp_pub_key);
+	//char string_array[46];
+	//sp_pub_key.toCharArray(string_array, sp_pub_key_length +1);
+	//int decodedLen = base64_dec_len(string_array, sp_pub_key_length);
+	int decodedLen = base64_dec_len(sp_pub_key, sp_pub_key_length);
+	char decoded[decodedLen];
+	base64_decode(decoded, sp_pub_key, sp_pub_key_length);
+	for(uint8_t kk = 0; kk < decodedLen; kk++){
+		smartphone_public_key[kk] = decoded[kk];
+		//Serial.print(smartphone_public_key[kk]);
+		//Serial.print(F(", "));
+	}
+	//Serial.println();
+
+}
+
+void dealWithEncryptedMsg(String enc_msg){
+	//Serial.print(F("Received encypted message: "));
+	//Serial.println(enc_msg);
+
+	uint8_t iv[12];
+	uint8_t encodedTextSize = enc_msg.length();
+	char encodedTextArray[100];
+	enc_msg.toCharArray(encodedTextArray, encodedTextSize+1);
+	uint8_t decodedLen = base64_dec_len(encodedTextArray, encodedTextSize);
+	char decoded[decodedLen];
+	base64_decode(decoded, encodedTextArray, encodedTextSize);
+	//Serial.println(decodedLen);
+	uint8_t cipheredTextValues[decodedLen-12];
+	//Serial.print(F("IV: "));
+	for(uint8_t j=0; j<12; j++){
+		iv[j] = decoded[j];
+		//Serial.print(iv[j]);
+		//Serial.print(F(", "));
+	}
+	//Serial.println();
+	//Serial.println(F("CIPHERED TEXT: "));
+	for(uint8_t k = 12; k < decodedLen; k++){
+		cipheredTextValues[k-12] = decoded[k];
+		//Serial.print(cipheredTextValues[k-12]);
+		//Serial.print(", ");
+	}
+	//Serial.println();
+	uint8_t outputBuffer[20];
+	GCM<AES256> gcm;
+	gcm.setKey(smartphone_public_key, sizeof(smartphone_public_key));
+	gcm.setIV(iv, sizeof(iv));
+	// technically don't need to use any intermediate memory and can just do:
+	// gcm.setIV(decoded, 12);
+	gcm.decrypt(outputBuffer, cipheredTextValues, decodedLen);
+	// one of these also works:
+	// gcm.decrypt(outputBuffer, decoded + 12, decodedLen);
+	// gcm.decrypt(outputBuffer, &decoded[12], decodedLen);
+	for(uint8_t k = 0; k<sizeof(outputBuffer); k++){
+		//Serial.print(char(outputBuffer[k]));
+	}
+	//Serial.println();
+	//dealWithDisconnection();
+}
+
+/*
+void dealWithData() {
+	while (altSerial.available() > 0) {
+		char char_received = altSerial.read();
+		//Serial.println(String("----> ") + char_received + String(" || ") + int(received_smartphone_pub_key));
+		if (char_received == '\n') {
+			Serial.println("-> ." + string_received + ".");
+			if (received_smartphone_pub_key) {
+				Serial.println("true");
+			}
+			else {
+				Serial.println("false");
+			}
+			if(!received_smartphone_pub_key){
+				received_smartphone_pub_key = true;
+				//Serial.println(String("ns: ") + int(received_smartphone_pub_key));
+				sendArduinoPubKey();
+				dealWithSmartphoneKey(string_received.substring(0, string_received.length() - 1));
+				generateSharedSecret();
+				Serial.println(F("sending ok to ble nano2.."));
+				altSerial.println(F("okay2"));
+				altSerial.flush();
+			}
+			else {
+				Serial.println(F("gottem"));
+				Serial.println(string_received);
+				//dealWithEncryptedMsg(string_received);
+				//generateECDHkeyValues();
+				Serial.println(F("gottem2"));
+			}
+			string_received = "";
+		}
+		else {
+			string_received += char_received;
+		}
+	}
+}
+*/
+
+
+void dealWithData2() {
+	char buffer[MAX_MSG_LEN];
+	int bufferIdx = 0;
+	// limpar o serial a seguir ao "break"
+	while (true) {
+		if (!altSerial.available())
+			continue;
+
+		char single_char = char(altSerial.read());
+		//Serial.println(String("----> ") + int(single_char));
+		// check for new line or carriage return
+		if (single_char == '\n') 
+			break;
+		
+		buffer[bufferIdx] = single_char;
+		bufferIdx += 1;
+	}
+
+	// null-terminate string
+	buffer[bufferIdx] = 0;
+		
+	//Serial.println(bufferIdx);
+	//String char_to_string = String(buffer);
+	//Serial.println(sizeof(buffer));
+	Serial.print(F("Recv: "));
+	Serial.println(buffer);
+	// Serial.println("---> ." + char_to_string.substring(0, bufferIdx)  + ".");
+	if (!received_smartphone_pub_key) {
+		received_smartphone_pub_key = true;
+		sendArduinoPubKey();
+		dealWithSmartphoneKey(buffer);
+		generateSharedSecret();
+		Serial.println(F("sending ok.."));
+		altSerial.println(F("okayy"));
+		altSerial.flush();
+	}
+	else {
+		Serial.println(F("gttm"));
+		Serial.println(buffer);
+		Serial.println(F("gttm2"));
+	}
+
+}
+
+void setup() {
+	Serial.begin(57600);
+	pinMode(pin_accept, OUTPUT);
+	pinMode(pin_decline, OUTPUT);
+
+	while (!Serial) {;}
+	Serial.println(F("hi!"));
+	
+	generateECDHkeyValues();
+	altSerial.begin(9600);
+}
+
+void loop() {
+	if (altSerial.available() > 0) {
+		dealWithData2();
+	}
+}
+
---
+++ blenano_peripheral/blenano_peripheral.ino
@@ -0,0 +1,276 @@
+
+#include <nRF5x_BLE_API.h>
+
+#define DEVICE_NAME       "NANO 1"
+#define TXRX_BUF_LEN      30
+// Create ble instance
+BLE                       ble;
+// Create a timer task000
+Ticker                    ticker1s;
+
+const int pin_out = D13;
+char ok_string1[] = "ok1";
+char ok_string2[] = "ok2";
+char ok_string3[] = "ok3";
+char ok_string4[] = "ok4";
+
+bool received_android_key = false;
+bool received_first_half = false;
+bool received_arduino_key = false;
+uint8_t number_of_key_parts = 0;
+uint8_t current_key_part = 0;
+String full_android_key = "";
+String full_arduino_key = "";
+
+uint8_t number_of_msg_parts = 0;
+uint8_t current_msg_part = 0;
+String full_encrypted_msg = "";
+bool received_encrypted_msg = false;
+bool received_first_part_msg = false;
+
+void blinky(void) {
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+  delay(100);
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+  delay(100);
+}
+
+void longblinky(void) {
+  digitalWrite(pin_out, HIGH);
+  delay(700);
+  digitalWrite(pin_out, LOW);
+  delay(300);
+  digitalWrite(pin_out, HIGH);
+  delay(700);
+  digitalWrite(pin_out, LOW);
+  delay(300);
+}
+
+// The uuid of service and characteristics
+static const uint8_t service1_uuid[] = { 0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars1_uuid[] = { 0x71, 0x3D, 0, 2, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars2_uuid[] = { 0x71, 0x3D, 0, 3, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars3_uuid[] = { 0x71, 0x3D, 0, 4, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+// Used in advertisement
+static const uint8_t uart_base_uuid_rev[] = { 0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71 };
+
+// Initialize value of chars0
+uint8_t chars_value[TXRX_BUF_LEN] = { 0 };
+
+// Create characteristic
+GattCharacteristic  characteristic1(service1_chars1_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
+GattCharacteristic  characteristic2(service1_chars2_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
+GattCharacteristic  characteristic3(service1_chars3_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
+GattCharacteristic *customServChars[] = { &characteristic1, &characteristic2, &characteristic3 };
+//Create service
+GattService         customService(service1_uuid, customServChars, sizeof(customServChars) / sizeof(GattCharacteristic *));
+
+//DeviceInformationService *deviceInfo;
+
+void dealWithDisconnection(){
+  received_arduino_key = false;
+  received_android_key = false;
+  received_first_half = false;
+  received_encrypted_msg = false;
+  received_first_part_msg = false;
+  number_of_key_parts = 0;
+  number_of_msg_parts = 0;
+  current_key_part = 0;
+  current_msg_part = 0;
+  full_encrypted_msg = "";
+  full_arduino_key = "";
+  full_android_key = "";
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+  }
+  ble.startAdvertising();
+}
+
+
+// Waiting to receive public key from arduino,
+// and send it to smartphone
+void wait_for_arduino_key(){
+  int n_tries = 20;
+  while(!Serial.available()){
+    delay(700);
+    n_tries -= 1;
+    if(n_tries == 0){
+      char coise[] = "failed1";
+      ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)&coise, sizeof(coise));
+      return;
+    }
+  }
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+    if (char_received == '\n') {
+      received_arduino_key = true;
+      for(uint8_t i=0; i<2; i++){
+        String i_part = full_arduino_key.substring(20*i, 20*(i+1));
+        char* to_send = new char[21];
+        i_part.toCharArray(to_send, 21);
+        ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)to_send, 20);
+        delay(75); 
+      }
+      uint8_t string_size = full_arduino_key.length();
+      String third_half = full_arduino_key.substring(40, string_size);
+      char* last_to_send = new char[string_size - 39];
+      third_half.toCharArray(last_to_send, string_size - 39);
+      ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)last_to_send, string_size - 40);
+      return;
+    }
+    else {
+      full_arduino_key += char_received;
+    }
+  }
+}
+
+//Waiting to receive an "ok" from the Arduino, in order to send the encrypted text from the smartphone
+void wait_for_arduino_ok(){
+  int n_tries = 20;
+  while(!Serial.available()){
+    delay(500);
+    n_tries -= 1;
+    if(n_tries == 0){
+      ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)&ok_string1, sizeof(ok_string1));
+      dealWithDisconnection();
+      return;
+    }
+  }
+  String string_to_send = "";
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+    string_to_send += char_received;
+  }
+  int string_size = string_to_send.length();
+  char* string2array = new char[string_size+1];
+  string_to_send.toCharArray(string2array, string_size+1);
+//  String aux = full_encrypted_msg.substring(0,20);
+  full_encrypted_msg = "hihellomynameisjoe1234";
+//  por 1 delay de 1 segundo
+// por flush em todos os "Serial" (not altSerial) no arduino
+  delay(40);
+  Serial.println(full_encrypted_msg);
+  Serial.flush();
+  delay(400);
+  dealWithDisconnection();
+  delay(400);
+  ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)string2array, string_size);
+}
+
+void disconnectionCallBack(const Gap::DisconnectionCallbackParams_t *params) {
+//  Serial.println("Restart advertising ");
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+  }
+  received_arduino_key = false;
+  received_android_key = false;
+  received_first_half = false;
+  received_encrypted_msg = false;
+  received_first_part_msg = false;
+  number_of_key_parts = 0;
+  number_of_msg_parts = 0;
+  current_key_part = 0;
+  current_msg_part = 0;
+  ble.startAdvertising();
+}
+
+
+void connectionCallBack(const Gap::ConnectionCallbackParams_t *params) {}
+
+
+void gattServerWriteCallBack(const GattWriteCallbackParams *Handler) {
+  uint8_t index;
+  String value_written = "";
+
+  for (index = 0; index<Handler->len; index++) {
+    value_written += char(Handler->data[index]);
+  }
+
+  if(!received_android_key){
+    if(!received_first_half){
+      char nop = value_written.charAt(0);
+      number_of_key_parts = nop - '0';
+      received_first_half = true;
+      current_key_part = 1;
+      full_android_key += value_written.substring(1,value_written.length());
+    }
+    else{
+      full_android_key += value_written;
+      current_key_part += 1;
+      if(current_key_part == number_of_key_parts){
+        received_android_key = true;
+        Serial.println(full_android_key);
+        full_android_key = "";
+        wait_for_arduino_key();
+        return;
+      }
+    }
+  }
+  else if(!received_encrypted_msg){
+    if(!received_first_part_msg){
+      char n_msg_parts = value_written.charAt(0);
+      number_of_msg_parts = n_msg_parts - '0';
+      received_first_part_msg = true;
+      current_msg_part = 1;
+      full_encrypted_msg += value_written.substring(1, value_written.length());
+    }
+    else{
+      full_encrypted_msg += value_written;
+      current_msg_part += 1;
+      if(current_msg_part == number_of_msg_parts){
+        received_encrypted_msg = true;
+//        Serial.println(full_encrypted_msg);
+//        full_encrypted_msg = "";
+        wait_for_arduino_ok();
+        return;
+      }
+    }
+  }
+  else{
+    Serial.println("naodeviachegaraqui");
+  }
+}
+
+
+void setAdvertisement(void) {
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+  // Add short name to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME, (const uint8_t *)"TXRX", 4);
+  // Add complete 128bit_uuid to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS, (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));
+  // Add complete device name to scan response data
+  ble.accumulateScanResponse(GapAdvertisingData::COMPLETE_LOCAL_NAME, (const uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME) - 1);
+}
+
+void setup() {
+  Serial.begin(9600);
+  //Serial.println(DEVICE_NAME);
+  pinMode(pin_out, OUTPUT);
+
+  // Init ble
+  ble.init();
+  ble.gap().onConnection(connectionCallBack);
+  ble.gap().onDisconnection(disconnectionCallBack);
+  ble.gattServer().onDataWritten(gattServerWriteCallBack);
+
+  // set advertisement
+  setAdvertisement();
+  ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+  ble.addService(customService);
+  ble.setDeviceName((const uint8_t *)DEVICE_NAME);
+  ble.setTxPower(4);
+  ble.setAdvertisingInterval(160);
+  ble.setAdvertisingTimeout(0);
+
+  ble.startAdvertising();
+}
+
+void loop() {
+  ble.waitForEvent();
+}
+
+
