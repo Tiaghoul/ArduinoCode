f082118f9fc7857d61a3cc13252e9991776be0be
Tiago Martins <tiaguinhomartins_@hotmail.com>
2018-07-01 23:37:37 +0100

    Simple DH exchange.

---
+++ dh_key_exchange/dh_key_exchange.ino
@@ -0,0 +1,218 @@
+#include <Base64.h>
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+const int pin_accept = 13;
+const int pin_decline = 8;
+
+String smartphonesArray[2] = {"uXUi27eQpTCOaB8DfHgD", "deeznuts"};
+String string_received = "";
+String string_open = "open";
+bool can_read = true;
+bool received_public = false;
+String full_encoded_msg = "";
+uint8_t two_halfs = 0;
+
+//public variable representing the shared secret key.
+String key_string;
+uint32_t k;
+uint32_t A;
+uint32_t a;
+//prime number 
+const uint32_t prime = 2147483647;
+//generator
+const uint32_t generator = 16807;
+
+uint32_t keyGen() {
+  //Seed the random number generator with a reading from an unconnected pin, I think this on analog pin 2
+  randomSeed(analogRead(2));
+
+  //return a random number between 1 and our prime .
+  return random(1, prime);
+}
+
+//code to compute the remainder of two numbers multiplied together.
+uint32_t mul_mod(uint32_t a, uint32_t b, uint32_t m) {
+
+
+  uint32_t result = 0; //variable to store the result
+  uint32_t runningCount = b % m; //holds the value of b*2^i
+
+  for (int i = 0; i < 32; i++) {
+
+    if (i > 0) runningCount = (runningCount << 1) % m;
+    if (bitRead(a, i)) {
+      result = (result%m + runningCount % m) % m;
+
+    }
+
+  }
+  return result;
+}
+
+//The pow_mod function to compute (b^e) % m that was given in the class files  
+uint32_t pow_mod(uint32_t b, uint32_t e, uint32_t m) {
+  uint32_t r;  // result of this function
+  uint32_t pow;
+  uint32_t e_i = e;
+  uint8_t i;
+
+  if (b == 0 || m == 0) {
+    return 0;
+  }
+
+  if (e == 0) {
+    return 1;
+  }
+
+  b = b % m;
+
+  pow = b;
+
+  r = 1;
+
+  while (e_i) {
+    if (e_i & 1) {
+      r = mul_mod(r, pow, m);//(r * pow) % m; 
+    }
+
+    pow = mul_mod(pow, pow, m);//(pow * pow) % m;
+
+    e_i = e_i >> 1;
+    i++;
+  }
+
+  return r;
+}
+
+
+
+void blink(int pin_out) {
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+  delay(250);
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+}
+
+void generateKeyValues() {
+  a = keyGen();
+  A = pow_mod(generator, a, prime);
+}
+
+void dealWithSmartphoneKey(String spKey) {
+  Serial.println();
+  Serial.println("---> dealing with smartphone key");
+  uint32_t smartPhoneKey = spKey.substring(0, spKey.length() - 1).toInt();
+  if (smartPhoneKey == 0) {
+    Serial.println("it's 0, returning now");
+    received_public = false;
+    mySerial.println("fail1");
+    return;
+  }
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("A = ");
+  Serial.println(A);
+  mySerial.println(String(A));
+  k = pow_mod(smartPhoneKey, a, prime);
+  key_string = String(k);
+  Serial.print("shared key = ");
+  Serial.println(key_string);
+  generateKeyValues();
+}
+
+void xorDecrypt(String encryptedMessage) {
+  two_halfs = 0;
+  Serial.println();
+  Serial.println("---> xoring and decripting: " + encryptedMessage);
+
+  
+  int input2len = encryptedMessage.length();
+  char* string_array = new char[input2len + 1];
+  encryptedMessage.toCharArray(string_array, input2len + 1);
+  int decodedlen = base64_dec_len(string_array, input2len);
+  char decoded[decodedlen];
+  base64_decode(decoded, string_array, input2len);
+  Serial.println(decoded);
+
+  String decryptedMessage = "";
+  for (uint8_t i = 0; i < decodedlen; i++) {
+    //decryptedMessage += char(encryptedMessage.charAt(i) ^ key_string.charAt(i % key_string.length()));
+    decryptedMessage += char(decoded[i] ^ key_string.charAt(i % key_string.length()));
+  }
+  decryptedMessage = decryptedMessage.substring(0, decryptedMessage.length() - 1);
+  Serial.print("decripted message : ");
+  Serial.println(decryptedMessage);
+  for (uint8_t i = 0; i < sizeof(smartphonesArray) / sizeof(String); i++) {
+    if (smartphonesArray[i].equals(decryptedMessage)){
+      Serial.println("SUCCESSSSSS");
+      mySerial.println("success");
+      blink(pin_accept);
+      return;
+    }
+  }
+
+  Serial.println("none of the IDs were right...");
+  mySerial.println("fail2");
+  blink(pin_decline);
+}
+
+void dealWithData() {
+  while (mySerial.available() > 0) {
+    char char_received = mySerial.read();
+    if (char_received == '\n') {
+      if (!received_public) {
+        Serial.println("| " + string_received + " |");
+        received_public = true;
+        dealWithSmartphoneKey(string_received);
+      }
+      else if (two_halfs == 0) {
+        Serial.println("|| " + string_received + " ||");
+        full_encoded_msg = string_received.substring(0, string_received.length() - 1);
+        two_halfs += 1;
+        mySerial.println("ok");
+      }
+      else if(two_halfs == 1){
+        Serial.println("||| " + string_received + " |||");
+        full_encoded_msg += string_received;
+        received_public = false;
+        xorDecrypt(full_encoded_msg);
+      }
+      string_received = "";
+    }
+    else {
+      string_received += char_received;
+    }
+  }
+  can_read = true;
+
+}
+
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(pin_accept, OUTPUT);
+  pinMode(pin_decline, OUTPUT);
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("START");
+  
+  generateKeyValues();
+
+  mySerial.begin(9600);
+}
+
+void loop() {
+  if (mySerial.available() > 0 && can_read) {
+    can_read = false;
+    dealWithData();
+  }
+}
+
+
---
+++ dh_key_exchange_blenano/dh_key_exchange_blenano.ino
@@ -0,0 +1,169 @@
+
+#include <nRF5x_BLE_API.h>
+
+#define DEVICE_NAME       "LED_1"
+#define TXRX_BUF_LEN      30
+// Create ble instance
+BLE                       ble;
+// Create a timer task000
+Ticker                    ticker1s;
+
+const int pin_out = D13;
+bool received_android_key = false;
+bool received_first_half = false;
+
+void blinky(void) {
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+  delay(100);
+  digitalWrite(pin_out, HIGH);
+  delay(250);
+  digitalWrite(pin_out, LOW);
+  delay(100);
+}
+
+void longblinky(void) {
+  digitalWrite(pin_out, HIGH);
+  delay(700);
+  digitalWrite(pin_out, LOW);
+  delay(300);
+  digitalWrite(pin_out, HIGH);
+  delay(700);
+  digitalWrite(pin_out, LOW);
+  delay(300);
+}
+
+// The uuid of service and characteristics
+static const uint8_t service1_uuid[] = { 0x71, 0x3D, 0, 0, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars1_uuid[] = { 0x71, 0x3D, 0, 2, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars2_uuid[] = { 0x71, 0x3D, 0, 3, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+static const uint8_t service1_chars3_uuid[] = { 0x71, 0x3D, 0, 4, 0x50, 0x3E, 0x4C, 0x75, 0xBA, 0x94, 0x31, 0x48, 0xF1, 0x8D, 0x94, 0x1E };
+// Used in advertisement
+static const uint8_t uart_base_uuid_rev[] = { 0x1E, 0x94, 0x8D, 0xF1, 0x48, 0x31, 0x94, 0xBA, 0x75, 0x4C, 0x3E, 0x50, 0, 0, 0x3D, 0x71 };
+
+// Initialize value of chars0
+uint8_t chars_value[TXRX_BUF_LEN] = { 0 };
+
+// Create characteristic
+GattCharacteristic  characteristic1(service1_chars1_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
+GattCharacteristic  characteristic2(service1_chars2_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ);
+GattCharacteristic  characteristic3(service1_chars3_uuid, chars_value, 1, TXRX_BUF_LEN, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY);
+GattCharacteristic *customServChars[] = { &characteristic1, &characteristic2, &characteristic3 };
+//Create service
+GattService         customService(service1_uuid, customServChars, sizeof(customServChars) / sizeof(GattCharacteristic *));
+
+//DeviceInformationService *deviceInfo;
+
+void disconnectionCallBack(const Gap::DisconnectionCallbackParams_t *params) {
+  //Serial.println("Restart advertising ");
+  String string_rec = "";
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+  }
+  received_android_key = false;
+  received_first_half = false;
+  ble.startAdvertising();
+}
+
+
+void connectionCallBack(const Gap::ConnectionCallbackParams_t *params) {
+  /*for(index=5; index > 0; index--) {
+  Serial.print(params->ownAddr[index], HEX);
+  Serial.print(" ");
+  }
+  Serial.print(params->ownAddr[0], HEX);
+  Serial.println(" ");*/
+}
+
+
+void gattServerWriteCallBack(const GattWriteCallbackParams *Handler) {
+  uint8_t index;
+  String value_written = "";
+  String string_received = "";
+
+  for (index = 0; index<Handler->len; index++) {
+    value_written += char(Handler->data[index]);
+  }
+  Serial.println(value_written);
+//  if(!received_android_key){
+//    received_android_key = true;
+//  }
+//  else if(!received_first_half){
+//    // tirar este elseif e por o arduino a responder sempre
+//    received_first_half = true;
+//    char coise[] = "ok";
+//    ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)&coise, sizeof(coise));
+//    return;
+//  }
+  
+  int n_tries = 20;
+  while (!Serial.available()) {
+    blinky();
+    n_tries -= 1;
+    if (n_tries == 0) {
+      char coise[] = "failed1";
+      ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)&coise, sizeof(coise));
+      return;
+    }
+  }
+
+  while (Serial.available() > 0) {
+    char char_received = Serial.read();
+    if (char_received == '\n') {
+//      Serial.println(string_received);
+      int string_size = string_received.length();
+      char* recebido = new char[string_size + 1];
+      string_received.toCharArray(recebido, string_size + 1);
+      ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)recebido, string_size);
+      return;
+    }
+    else {
+      string_received += char_received;
+    }
+  }
+
+  char coise[] = "failed2";
+  ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), (uint8_t *)&coise, sizeof(coise));
+}
+
+
+void setAdvertisement(void) {
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+  // Add short name to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME, (const uint8_t *)"TXRX", 4);
+  // Add complete 128bit_uuid to advertisement
+  ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS, (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));
+  // Add complete device name to scan response data
+  ble.accumulateScanResponse(GapAdvertisingData::COMPLETE_LOCAL_NAME, (const uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME) - 1);
+}
+
+void setup() {
+  Serial.begin(9600);
+  //Serial.println(DEVICE_NAME);
+  pinMode(pin_out, OUTPUT);
+
+  // Init ble
+  ble.init();
+  ble.gap().onConnection(connectionCallBack);
+  ble.gap().onDisconnection(disconnectionCallBack);
+  ble.gattServer().onDataWritten(gattServerWriteCallBack);
+  // procurar mtu stuff
+
+  // set advertisement
+  setAdvertisement();
+  ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+  ble.addService(customService);
+  ble.setDeviceName((const uint8_t *)DEVICE_NAME);
+  ble.setTxPower(4);
+  ble.setAdvertisingInterval(160);
+  ble.setAdvertisingTimeout(0);
+
+  ble.startAdvertising();
+}
+
+void loop() {
+  ble.waitForEvent();
+}
+
+
